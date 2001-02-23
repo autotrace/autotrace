12d92ad7d7f95422b0e244d6d74868243369f938
Masatake YAMATO <yamato@redhat.com>
2001-02-23 11:44:47 +0000

    Update Autofig relaetd files.

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,14 @@
 #! /bin/sh
+
+(autofig --version) < /dev/null > /dev/null 2>&1 || {
+  echo
+  echo "**Error**: You must have \`autofig' installed to compile Autotrace."
+  echo "See HACKING in autotrace soruce directory."
+  echo "Abort..."
+  exit 1
+}
+
 autofig autotrace-config.af
 aclocal $ACLOCAL_FLAGS
 automake -a
 autoconf
-
-
