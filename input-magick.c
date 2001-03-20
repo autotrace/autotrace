6953de1273a42f1457fbc2562a50f48eac15e1a6
Masatake YAMATO <yamato@redhat.com>
2001-03-20 11:47:27 +0000

    2001-03-20 Masatake YAMATO <yamato@redhat.com>
    	* configure.in: Check ImageMagick version is higher
    	than 5.2.1. Use AC_MSG_WARN instead of AC_MSG_ERROR
    	if ImageMagick is not found. The user can build
    	autotrace without ImageMagick.
    
    2001-03-15  Martin Weber <martweb@gmx.net>
    
    	Message-ID: <3AB26698.DFC2341D@gmx.net>.
    	* input-magick.c (magick_load_image):
    	dropped the support of versions older than 5.2.1.

--- input-magick.c
+++ input-magick.c
@@ -6,53 +6,31 @@
 #include <magick/magick.h>
 #include "input-magick.h"
 #include "bitmap.h"
-#if MagickLibVersion < 0x500
-#include "xstd.h"
-#endif
 
 bitmap_type magick_load_image(string filename)
 {
   Image *image;
   ImageInfo *image_info;
-#if MagickLibVersion >= 0x521
   ImageType image_type;
-#endif
   unsigned int i,j,point,np,runcount;
   bitmap_type bitmap;
-#if MagickLibVersion < 0x500
-  RunlengthPacket* pixel;
-#else
   PixelPacket p;
   PixelPacket *pixel=&p;
-#endif
-#if MagickLibVersion >= 0x521
   ExceptionInfo exception;
   MagickIncarnate("");
   GetExceptionInfo(&exception);
   image_info=CloneImageInfo((ImageInfo *) NULL);
-#else
-  XMALLOC(image_info, sizeof(&image_info));
-#endif
   (void) strcpy(image_info->filename,filename);
 
-#if MagickLibVersion < 0x521
-  image=ReadImage(image_info);
-#else
   image=ReadImage(image_info,&exception);
   if (image == (Image *) NULL)
-#endif
-#if (MagickLibVersion >= 0x521) && (MagickLibVersion <= 0x525)
+#if (MagickLibVersion <= 0x525)
     MagickError(exception.severity,exception.message,exception.qualifier);
-#endif
-#if MagickLibVersion >= 0x526
+#else
     MagickError(exception.severity,exception.reason,exception.description);
 #endif
-#if MagickLibVersion < 0x521
-  if(IsGrayImage (image) || IsMonochromeImage (image))
-#else
   image_type=GetImageType(image);
   if(image_type == BilevelType || image_type == GrayscaleType)
-#endif
     np=1;
   else
     np=3;
@@ -62,27 +40,13 @@ bitmap_type magick_load_image(string filename)
   BITMAP_HEIGHT (bitmap)=image->rows;
   BITMAP_BITS (bitmap)=(unsigned char*)malloc(np*image->columns*image->rows);
 
-#if MagickLibVersion < 0x521
-  pixel=image->pixels;
-#endif
-
   for(j=0,runcount=0,point=0;j<image->rows;j++)
     for(i=0;i<image->columns;i++) {
-#if MagickLibVersion < 0x521
-      if(runcount)
-	runcount--;
-      else {
-	runcount=pixel->length;
-	pixel++;
-      }
-#else
       p=GetOnePixel(image,i,j);
-#endif
-
       bitmap.bitmap[point++]=pixel->red; /* if gray: red=green=blue */
       if(np==3) {
-	bitmap.bitmap[point++]=pixel->green;
-	bitmap.bitmap[point++]=pixel->blue;
+        bitmap.bitmap[point++]=pixel->green;
+        bitmap.bitmap[point++]=pixel->blue;
       }
     }
   return(bitmap);
