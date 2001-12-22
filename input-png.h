a9eecaf120b97aa0c5b2b25625e3a409b0fca0b0
Masatake YAMATO <yamato@redhat.com>
2001-12-22 16:07:42 +0000

    string->at_string.

--- input-png.h
+++ input-png.h
@@ -1,10 +1,10 @@
-/* $Id: input-png.h,v 1.1 2000/10/11 14:00:26 masata-y Exp $ */
+/* $Id: input-png.h,v 1.2 2001/12/22 16:07:42 masata-y Exp $ */
 /* input-png.h: import png files. */
 #ifndef INPUT_PNG_H
 #define INPUT_PNG_H
 
 #include "input.h"
 
-bitmap_type png_load_image(string filename);
+bitmap_type png_load_image(at_string filename);
 
 #endif /* not INPUT_PNG_H */
