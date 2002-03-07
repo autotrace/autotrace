1c429a0d1703e7b4aa1085479e9a70ced7812f33
Martin Weber <martweb@gmx.net>
2002-03-07 02:17:26 +0000

    2002-03-06 Martin Weber <martweb@gmx.net>
    	* curve.c: Corrected copyright notice
    
    	* fit.c: Likewise.
    
    	Message-ID: <3C84F316.2DB5B7A1@gmx.net>.
    
    2002-03-06  Martin Weber <martweb@gmx.net>
    
    	* autotrace.vcproj: Linking with certain ImageMagick
    	versions didn't work correctly.
    
    	* autotrace.dsp: Likewise.
    
    	Message-ID: <3C84F0C1.4C989134@gmx.net>.

--- curve.c
+++ curve.c
@@ -1,4 +1,22 @@
-/* curve.c: operations on the lists of pixels and lists of curves. */
+/* curve.c: operations on the lists of pixels and lists of curves.
+
+   The code was partially derived from limn.
+
+   Copyright (C) 1992 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
 #include "logreport.h"
 #include "curve.h"
