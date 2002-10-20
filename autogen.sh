73a4f20f468c3836ca521c48d89fe6276df651f5
Masatake YAMATO <yamato@redhat.com>
2002-10-20 07:29:27 +0000

    * autogen.sh: checked aclocal-1.4, automake-1.4 and autoconf-2.53 are available or not.
    * autotrace.h: defined N_ and _ only if they are
    not defined.

--- autogen.sh
+++ autogen.sh
@@ -9,7 +9,24 @@
 }
 
 autofig autotrace-config.af
-aclocal $ACLOCAL_FLAGS
+
+if (aclocal-1.4 --version)  < /dev/null > /dev/null 2>&1; then
+    aclocal-1.4 $ACLOCAL_FLAGS
+else
+    aclocal $ACLOCAL_FLAGS
+fi
+
 autoheader
-automake -a
-autoconf
+
+if (automake-1.5 --version) < /dev/null > /dev/null 2>&1; then
+    automake-1.5 -a
+else
+    automake -a
+fi
+
+if (autoconf-2.53 --version) < /dev/null > /dev/null 2>&1; then
+    autoconf-2.53
+else
+    autoconf
+fi
+
--- autotrace.h
+++ autotrace.h
@@ -27,8 +27,12 @@
 extern "C" {
 #endif /* __cplusplus */
 
+#ifndef _
 #define _(x) x
+#endif
+#ifndef N_
 #define N_(x) x
+#endif /* Not def: N_ */
  
 /* ===================================================================== *
  * Typedefs
