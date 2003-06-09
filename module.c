e8f11fbc655de2843fd0bb8b4e29a16ab13cf914
Masatake YAMATO <yamato@redhat.com>
2003-06-09 23:13:48 +0000

    * module.c (at_module_init): Use at_input_add_handler_full for "PBM", "PNM", "PGM" and "PPM". Added some preprocessor tricks to deal with ImageMagick. (at_module_init): Use at_input_add_handler_full to deal with ImageMagick.

--- module.c
+++ module.c
@@ -46,34 +46,18 @@ at_module_init (void)
    For a while, these are staticly added. */
   
 #ifdef HAVE_LIBPNG  
-  at_input_add_handler ("PNG", "Portable network graphics",      input_png_reader);
+  at_input_add_handler      ("PNG", "Portable network graphics",      input_png_reader);
 #endif
-  at_input_add_handler ("TGA", "Truevision Targa image",         input_tga_reader);
-  at_input_add_handler ("PBM", "Portable bitmap format",         input_pnm_reader);
-  at_input_add_handler ("PNM", "Portable anymap format",         input_pnm_reader);
-  at_input_add_handler ("PGM", "Portable graymap format",        input_pnm_reader);
-  at_input_add_handler ("PPM", "Portable pixmap format",         input_pnm_reader);
-  at_input_add_handler ("BMP", "Microsoft Windows bitmap image", input_bmp_reader);
+  at_input_add_handler      ("TGA", "Truevision Targa image",         input_tga_reader);
+  at_input_add_handler      ("BMP", "Microsoft Windows bitmap image", input_bmp_reader);
+
+  at_input_add_handler_full ("PBM", "Portable bitmap format",  input_pnm_reader, 0, "PBM", NULL);
+  at_input_add_handler_full ("PNM", "Portable anymap format",  input_pnm_reader, 0, "PNM", NULL);
+  at_input_add_handler_full ("PGM", "Portable graymap format", input_pnm_reader, 0, "PGM", NULL);
+  at_input_add_handler_full ("PPM", "Portable pixmap format",  input_pnm_reader, 0, "PPM", NULL);
+
 
 #if HAVE_MAGICK  
-
-#if (MagickLibVersion < 0x0540)
-#define AT_MAGICK_INFO_TYPE_MODIFIER     const
-#else  /* (MagickLibVersion < 0x0540) */
-#define AT_MAGICK_INFO_TYPE_MODIFIER 
-#endif	/* (MagickLibVersion < 0x0540)*/
-
-#if (MagickLibVersion < 0x0538)
-#define AT_MAGICK_INITIALIZER()     MagickIncarnate("")
-#else  /* (MagickLibVersion < 0x0538) */
-#define AT_MAGICK_INITIALIZER()     InitializeMagick("")
-#endif	/* (MagickLibVersion < 0x0538) */
-
-#if (MagickLibVersion < 0x0537)
-#define AT_MAGICK_SUFFIX_FIELD_NAME tag
-#else  /* (MagickLibVersion < 0x0537) */
-#define AT_MAGICK_SUFFIX_FIELD_NAME name
-#endif	/* (MagickLibVersion < 0x0537) */
 
 #if (MagickLibVersion < 0x0534)
 #define AT_MAGICK_SET_INFO(X) X = GetMagickInfo(NULL)
@@ -85,6 +69,26 @@ at_module_init (void)
       X = GetMagickInfo("*", &exception);	\
   } while (0)
 #endif	/* (MagickLibVersion < 0x0534) */
+
+#if (MagickLibVersion < 0x0537)
+#define AT_MAGICK_SUFFIX_FIELD_NAME tag
+#else  /* (MagickLibVersion < 0x0537) */
+#define AT_MAGICK_SUFFIX_FIELD_NAME name
+#endif	/* (MagickLibVersion < 0x0537) */
+
+#if (MagickLibVersion < 0x0538)
+#define AT_MAGICK_INITIALIZER()     MagickIncarnate("")
+#else  /* (MagickLibVersion < 0x0538) */
+#define AT_MAGICK_INITIALIZER()     InitializeMagick("")
+#endif	/* (MagickLibVersion < 0x0538) */
+
+
+#if (MagickLibVersion < 0x0540)
+#define AT_MAGICK_INFO_TYPE_MODIFIER     const
+#else  /* (MagickLibVersion < 0x0540) */
+#define AT_MAGICK_INFO_TYPE_MODIFIER 
+#endif	/* (MagickLibVersion < 0x0540)*/
+
   {
     ExceptionInfo exception;
 
@@ -99,11 +103,15 @@ at_module_init (void)
     while (info)
       {
 	if (info->AT_MAGICK_SUFFIX_FIELD_NAME && info->description)
-	  at_input_add_handler(info->AT_MAGICK_SUFFIX_FIELD_NAME,
-			       info->description,
-			       input_magick_reader);
+	  at_input_add_handler_full(info->AT_MAGICK_SUFFIX_FIELD_NAME,
+				    info->description,
+				    input_magick_reader,
+				    0,
+				    info->AT_MAGICK_SUFFIX_FIELD_NAME,
+				    NULL);
 	info = info->next ;
       }
   }
 #endif
+  return 0;
 }
