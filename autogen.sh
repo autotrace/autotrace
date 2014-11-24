7549cd12f9ba04f351b0598adc8f7c602296f531
Khaled Hosny <khaledhosny@eglug.org>
2014-11-24 13:11:54 +0200

    Cleanup autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,45 +1,21 @@
 #! /bin/sh
 
-srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
-${srcdir}/tools-version.sh
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
 
-(autofig --version) < /dev/null > /dev/null 2>&1 || {
-  echo
-  echo "**Error**: You must have \`autofig' installed to compile Autotrace."
-  echo "See HACKING in autotrace soruce directory."
-  echo "Abort..."
-  exit 1
+cd $srcdir
+
+echo -n "checking for pkg-config... "
+which pkg-config || {
+	echo "*** No pkg-config found, please install it ***"
+	exit 1
 }
 
-if (aclocal-1.5 --version)  < /dev/null > /dev/null 2>&1; then
-    aclocal-1.5 $ACLOCAL_FLAGS
-else
-    aclocal $ACLOCAL_FLAGS
-fi
-
-autoheader
-
-if (automake-1.5 --version) < /dev/null > /dev/null 2>&1; then
-    automake-1.5 -a
-else
-    automake -a
-fi
-
-libtoolize --copy --force
-
-if (autoconf-2.53 --version) < /dev/null > /dev/null 2>&1; then
-    autoconf-2.53
-else
-    autoconf
-fi
-
-echo "checking for glib-gettextize"
-(glib-gettextize --version) < /dev/null > /dev/null 2>&1 || {
-    echo
-    echo "  You must have glib-gettextize installed to compile autotrace"
-    echo "  glib-gettextize is part of glib-2.0, so you should already"
-    echo "  have it. Make sure it is in your PATH."
-    exit 1
+echo -n "checking for autoreconf... "
+which autoreconf || {
+	echo "*** No autoreconf found, please install it ***"
+	exit 1
 }
-glib-gettextize --copy --force
+
+echo "running autoreconf --force --install --verbose"
+autoreconf --force --install --verbose || exit $?
