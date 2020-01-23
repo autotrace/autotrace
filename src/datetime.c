77d52cfeb4ccabad89ebe3fa5a5d612ab9e4802a
Han Mertens <hanmertens@outlook.com>
2020-01-24 00:01:00 +0100

    Prevent too small time_string allocation
    Depending on locale, the formatted date and time may not fit in the
    allocated time_string buffer, e.g. when the time zone is also included.
    Since the allocation is short-lived anyway, a somewhat larger buffer
    size was chosen.

--- src/datetime.c
+++ src/datetime.c
@@ -4,16 +4,16 @@
 
 char *at_time_string(void)
 {
-  char *time_string = calloc(sizeof(char), 25);
+  char *time_string = calloc(sizeof(char), 50);
   time_t t = time(NULL);
 #if HAVE_LOCALTIME_R
   struct tm newtime;
 
   localtime_r(&t, &newtime);
-  strftime(time_string, 25, "%c", &newtime);
+  strftime(time_string, 50, "%c", &newtime);
 #else
   // the mingw-w64 toolchain does not have localtime_r()
-  strftime(time_string, 25, "%c", localtime(&t));
+  strftime(time_string, 50, "%c", localtime(&t));
 #endif
 
   return time_string;
